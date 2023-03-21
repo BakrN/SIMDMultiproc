e1479160b5b981314e73ad329416e8fe44afe96b
Samuel Tocci <samuel.tocci@student.kuleuven.be>
2023-03-21 18:07:55 +0100

    vector index bug fixed

--- software/Product.c
+++ software/Product.c
@@ -7,8 +7,6 @@
 #include <stdio.h>
 
 int id_count;
-int curr_size = VECTORSIZE;
-
 
 
 product_node_t* product_create(matrix_t *m, vector_t *v){
@@ -23,7 +21,6 @@ product_node_t* product_node_create(int parent_id, matrix_t *m, vector_t *v){
     node->parent_id = parent_id;
     node->m = m;
     node->v = v;
-    curr_size = vector_getLength(&v);
     return node;
 }
 
@@ -39,14 +36,14 @@ void product_decompose(product_node_t **pn){
         matrix_t ** m_decomp = matrix_2decompose(&(*pn)->m);
         vector_t ** v_decomp = vector_2decompose(&(*pn)->v);
 
-        int vindex = VECTORSIZE + 2*VECTORSIZE/(curr_size) + vector_getDataStart(&v_decomp[0])/2;
-        int tindex = VECTORSIZE/curr_size;
+        int vindex = VECTORSIZE + 2*VECTORSIZE/(length) + vector_getDataStart(&v_decomp[0])/2;
+        int tindex = VECTORSIZE/length;
 
-        if(VECTORSIZE == curr_size){
+        if(VECTORSIZE == length){
             vindex = VECTORSIZE + vector_getDataStart(&v_decomp[0]);
             tindex = 0;
         }
-        printf("\n \033[0;33m ##### vindex [%d] = %d + %d + %d  ##### \033[0m\n,", vindex, VECTORSIZE, 2*VECTORSIZE/curr_size,vector_getDataStart(&v_decomp[0])/2);
+        printf("\n \033[0;33m ##### vindex [%d] = %d + %d + %d  ##### \033[0m\n,", vindex, VECTORSIZE, 2*VECTORSIZE/length,vector_getDataStart(&v_decomp[0])/2);
 
         command_create(0,id_count, (*pn)->parent_id, matrix_getDataCenter(&m_decomp[0]) - tindex + length/2 +1, matrix_getDataCenter(&m_decomp[1]),-1, matrix_getDataCenter(&m_decomp[0]), length/2);
         command_create(0,id_count +1, (*pn)->parent_id, matrix_getDataCenter(&m_decomp[2]) + tindex - length/2 -1, matrix_getDataCenter(&m_decomp[1]),-1, matrix_getDataCenter(&m_decomp[2]), length/2);
